381d660193c2b9c274f39a9ff8669d25040d3773
ayyaz10 <codementee@outlook.com>
2025-01-15 15:27:51 +0500

    update

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const fetch = require("node-fetch");
 const ServerlessHttp = require("serverless-http");
 const cors = require("cors");
